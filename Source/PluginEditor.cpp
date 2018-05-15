0b636c8b68f563ebcf45b348fa1342c00aea2278
xenakios <xenakios@gmail.com>
2018-05-15 19:27:38 +0300

    Made the XY pad a separate component class

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -13,19 +13,14 @@
 
 //==============================================================================
 BinauralSpatAudioProcessorEditor::BinauralSpatAudioProcessorEditor (BinauralSpatAudioProcessor& p)
-    : AudioProcessorEditor (&p), processor (p)
+    : AudioProcessorEditor (&p), processor (p), m_xy(p)
 {
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
-    setSize (400, 300);
+	addAndMakeVisible(&m_xy);
+	setResizable(true, true);
+	setSize (400, 400);
 	startTimer(20);
-	m_xpos0 = processor.m_state.getParameterAsValue("pos0x");
-	m_xpos0.addListener(this);
-	m_ypos0 = processor.m_state.getParameterAsValue("pos0y");
-	m_ypos0.addListener(this);
-	m_zpos0 = processor.m_state.getParameterAsValue("pos0z");
-	m_zpos0.addListener(this);
-
+	
+	
 }
 
 BinauralSpatAudioProcessorEditor::~BinauralSpatAudioProcessorEditor()
@@ -35,19 +30,12 @@ BinauralSpatAudioProcessorEditor::~BinauralSpatAudioProcessorEditor()
 //==============================================================================
 void BinauralSpatAudioProcessorEditor::paint (Graphics& g)
 {
-	g.fillAll(Colours::black);
-	g.setColour(Colours::white);
-	float xcor = jmap<float>(m_xpos0.getValue(), -1.0, 1.0, 0.0, getWidth());
-	float ycor = jmap<float>(m_ypos0.getValue(), -1.0, 1.0, 0.0, getHeight());
-	float zsize = jmap<float>(m_zpos0.getValue(), -1.0, 1.0, 1.0, 50.0);
-	g.fillEllipse(xcor-zsize/2.0, ycor-zsize/2.0, zsize, zsize);
-	g.drawText(String(m_valuechangecount), 10, 10, 100, 50, Justification::centred);
+	g.fillAll(Colours::lightgrey);
 }
 
 void BinauralSpatAudioProcessorEditor::resized()
 {
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
+	m_xy.setBounds(0, 0, getWidth(), getHeight() - 20);
 }
 
 void BinauralSpatAudioProcessorEditor::timerCallback()
@@ -55,20 +43,30 @@ void BinauralSpatAudioProcessorEditor::timerCallback()
 	//repaint();
 }
 
-void BinauralSpatAudioProcessorEditor::valueChanged(Value & value)
+XYPadComponent::XYPadComponent(BinauralSpatAudioProcessor & p) : m_processor(p)
+{
+	m_xpos0 = m_processor.m_state.getParameterAsValue("pos0x");
+	m_xpos0.addListener(this);
+	m_ypos0 = m_processor.m_state.getParameterAsValue("pos0y");
+	m_ypos0.addListener(this);
+	m_zpos0 = m_processor.m_state.getParameterAsValue("pos0z");
+	m_zpos0.addListener(this);
+}
+
+void XYPadComponent::valueChanged(Value & value)
 {
 	++m_valuechangecount;
 	repaint();
 }
 
-void BinauralSpatAudioProcessorEditor::mouseDrag(const MouseEvent & ev)
+void XYPadComponent::mouseDrag(const MouseEvent & ev)
 {
 	float zsize = jmap<float>(m_zpos0.getValue(), -1.0, 1.0, 1.0, 50.0);
-	m_xpos0 = jlimit<float>(-1.0,1.0, jmap<float>(ev.x, 0.0, getWidth(), -1.0, 1.0));
-	m_ypos0 = jlimit<float>(-1.0,1.0, jmap<float>(ev.y, 0.0, getHeight(), -1.0, 1.0));
+	m_xpos0 = jlimit<float>(-1.0, 1.0, jmap<float>(ev.x, 0.0, getWidth(), -1.0, 1.0));
+	m_ypos0 = jlimit<float>(-1.0, 1.0, jmap<float>(ev.y, 0.0, getHeight(), -1.0, 1.0));
 }
 
-void BinauralSpatAudioProcessorEditor::mouseWheelMove(const MouseEvent & ev, const MouseWheelDetails & det)
+void XYPadComponent::mouseWheelMove(const MouseEvent & ev, const MouseWheelDetails & det)
 {
 	float curval = m_zpos0.getValue();
 	if (det.deltaY < 0.0)
@@ -76,4 +74,15 @@ void BinauralSpatAudioProcessorEditor::mouseWheelMove(const MouseEvent & ev, con
 	if (det.deltaY > 0.0)
 		curval += 0.05f;
 	m_zpos0 = jlimit(-1.0f, 1.0f, curval);
+}
+
+void XYPadComponent::paint(Graphics & g)
+{
+	g.fillAll(Colours::black);
+	g.setColour(Colours::white);
+	float xcor = jmap<float>(m_xpos0.getValue(), -1.0, 1.0, 0.0, getWidth());
+	float ycor = jmap<float>(m_ypos0.getValue(), -1.0, 1.0, 0.0, getHeight());
+	float zsize = jmap<float>(m_zpos0.getValue(), -1.0, 1.0, 2.0, 50.0);
+	g.fillEllipse(xcor - zsize / 2.0, ycor - zsize / 2.0, zsize, zsize);
+	g.drawText(String(m_valuechangecount), 10, 10, 100, 50, Justification::centred);
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -13,10 +13,23 @@
 #include "../JuceLibraryCode/JuceHeader.h"
 #include "PluginProcessor.h"
 
-//==============================================================================
-/**
-*/
-class BinauralSpatAudioProcessorEditor  : public AudioProcessorEditor, public Timer, public Value::Listener
+class XYPadComponent : public Component, public Value::Listener
+{
+public:
+	XYPadComponent(BinauralSpatAudioProcessor& p);
+	void valueChanged(Value& value) override;
+	void mouseDrag(const MouseEvent& ev) override;
+	void mouseWheelMove(const MouseEvent& ev, const MouseWheelDetails& det) override;
+	void paint(Graphics& g) override;
+private:
+	BinauralSpatAudioProcessor& m_processor;
+	Value m_xpos0;
+	Value m_ypos0;
+	Value m_zpos0;
+	int m_valuechangecount = 0;
+};
+
+class BinauralSpatAudioProcessorEditor  : public AudioProcessorEditor, public Timer
 {
 public:
     BinauralSpatAudioProcessorEditor (BinauralSpatAudioProcessor&);
@@ -26,16 +39,11 @@ public:
     void paint (Graphics&) override;
     void resized() override;
 	void timerCallback() override;
-	void valueChanged(Value& value) override;
-	void mouseDrag(const MouseEvent& ev) override;
-	void mouseWheelMove(const MouseEvent& ev, const MouseWheelDetails& det) override;
+	
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     BinauralSpatAudioProcessor& processor;
-	Value m_xpos0;
-	Value m_ypos0;
-	Value m_zpos0;
-	int m_valuechangecount = 0;
+	XYPadComponent m_xy;
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (BinauralSpatAudioProcessorEditor)
 };
